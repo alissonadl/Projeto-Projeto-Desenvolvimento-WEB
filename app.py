9a28f1f7da9b19f99d870d45992723fff0a76579
Alisson Araújo de Lima <160548927+alissonadl@users.noreply.github.com>
2024-08-04 22:26:36 +0000

    adicionando rotas

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template("home.html")
+@app.route("/")
+def inicio():
+    return render_template("inicio.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/buscar")
+def buscar():
+    return render_template("buscar.html")
+
+#Rotas de cadastrar novo animal e listar animais já cadastrados.
