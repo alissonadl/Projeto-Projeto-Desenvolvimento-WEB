3c03920320ba1ad6991e050442583cb1e271e96c
Alisson Araújo de Lima <160548927+alissonadl@users.noreply.github.com>
2024-08-20 00:00:14 +0000

    Adicionado conexão com o banco

---
+++ .flaskenv
@@ -0,0 +1,6 @@
+FLASK_APP =app:app
+SECRET_KEY = senha123
+DB_USERNAME = desweb2024
+DB_PASSWORD = D3sw3b1frn!
+DB_HOST = weblic2024.mysql.database.azure.com
+DB_DATABASE = desweb2024_alisson
--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 from flask import Flask, render_template, redirect, url_for
+from database import db
+import os #Biblioteca para ler arquivos como se fosse um "Sistema Operacional".
 
 app = Flask(__name__)
+#implementando "senha". Necessário para banco de dados; {
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+#}
 
-@app.route("/")
+#Importando os dados do falskenv(Arquivo privado){ 
+username = os.getenv("DB_USERNAME")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+mydb = os.getenv("DB_DATABASE")
+#}
+
+#Criando a conexão o caminho da conexão com o banco de dados {
+conexao = f"mysql+pymysql://{username}:{password}@{host}/{mydb}"
+app.config["SQLALCHEMY_DATABASE_URI"] = conexao
+#}
+
+@app.route("/") #Página raíz.
 def inicio():
     return render_template("inicio.html")
 
