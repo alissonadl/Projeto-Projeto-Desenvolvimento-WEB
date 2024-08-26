60696df0028304b1090b3e27749a81673af45f36
Alisson Araújo de Lima <160548927+alissonadl@users.noreply.github.com>
2024-08-26 23:18:41 +0000

    CRUD

--- app.py
+++ app.py
@@ -37,20 +37,52 @@ def login():
 def buscar():
     return render_template("buscar.html")
 
-@app.route("/cadastrar-se")
+@app.route("/cadastrar-animal")
+def cadastrar_animal():
+    return render_template("cadastro_animal.html")
+
+@app.route("/cadastrar-usuario")
 def cadastrar_usuario():
-    #add_new_user() ESSA FUNÇÃO ADICIONA O USUARIO. DA MANEIRA COMO ESTÁ ELE APENAS ADICIONA O USUARIO QUANDO A PÁGINA É EXIBIDA.
+    add_new_user() #ESSA FUNÇÃO ADICIONA O USUARIO NO BANCO, DA MANEIRA COMO ESTÁ ELE APENAS ADICIONA O USUARIO QUANDO A PÁGINA É EXIBIDA.
+    #Função mostrada abaixo
     return render_template("cadastro_usuario.html")
 
-#Função de cadastrar usuario no banco de dados. {}
+#Função de cadastrar usuario no banco de dados. {
+#IMPORTANTE: ISSO SERIA O "C" DO C.R.U.D. Ou seja, Create.
 def add_new_user():
     novo_usuario = Usuario("Usuario de teste", "email@teste.com","Senha#123")
     db.session.add(novo_usuario)
     db.session.commit()
-    return("Usuário cadastrado com sucesso!")
 #}
 
-@app.route("/cadastrar-animal")
-def cadastrar_animal():
-    return render_template("cadastro_animal.html")
+#Função para receber/recuprar dados do banco. {
+#IMPORTANTE: ISSO SERIA O "R" DO C.R.U.D. Ou seja, Recovery
+@app.route("/teste_recovery")
+def recovery_user():
+    user = Usuario.query.get("email@teste.com")#Precisa informar a primary key da tupla. Isso recebe os dados da tupla para essa variável.
+    teste_msg = f"{user.username}, {user.email}"
+    return teste_msg
+#}
+
+#Função para atualizar informações do banco. {
+#IMPORTANTE: ISSO SERIA O "U" DO C.R.U.D. Ou seja, Update
+@app.route("/teste_update")
+def update_user():
+    user = Usuario.query.get("email@teste.com")#Precisa informar a primary key da tupla. Isso recebe os dados da tupla para essa variável.
+    user.username = "Update Sucessful"
+    db.session.add(user) #Adiciona as informações atualizadas no banco
+    db.session.commit() #Dá commit no banco, efetivando as modificações
+    teste_msg = f"{user.username}, {user.email}"
+    return teste_msg
+#}
+
+#Função para deletar informações do banco. {
+#IMPORTANTE: ISSO SERIA O "D" DO C.R.U.D. Ou seja, Delete
+@app.route("/teste_delete")
+def user_delete():
+    user = Usuario.query.get("email@teste.com")#Precisa informar a primary key da tupla. Isso recebe os dados da tupla para essa variável.
+    db.session.delete(user) #Remove a tupla do banco
+    db.session.commit() #Dá commit no banco, efetivando as modificações
+    teste_msg = Usuario.query.all()
+    return teste_msg
 #}
